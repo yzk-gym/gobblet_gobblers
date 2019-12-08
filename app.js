76663fecdaa23867b31dc18faa78af9e3657c8a8
yuya <siren.night43@gmail.com>
2019-12-08 17:40:44 +0900

    ソケット通信の実装

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var buttleRouter = require('./routes/battle');
+var battleRouter = require('./routes/battle');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/battle', buttleRouter);
+app.use('/battle', battleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
